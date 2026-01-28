6c57685a185b5594d375f76327a4b6096db24e88
Gilvan Pedro <87739497+GilvanPedro@users.noreply.github.com>
2026-01-28 14:13:19 -0300

    Update Main.java

--- RoletaRussa/src/main/java/br/com/Main.java
+++ RoletaRussa/src/main/java/br/com/Main.java
@@ -6,36 +6,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         List<Integer> balas = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        double rand = Math.random();
-        int roleta = (int)(rand*8)+1;
-        int escolha = 0;
-
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 6; i++) {
             balas.add(i + 1);
         }
 
-        while(escolha != roleta && balas.size() != 1){
+        int roleta = (int) (Math.random() * 6) + 1;
+        int escolha = 0;
+
+        while (escolha != roleta && balas.size() > 1) {
 
             System.out.println("Escolha uma bala: " + balas);
             System.out.print("Escolha: ");
-            escolha = sc.nextInt();
 
-            for (int i = 0; i < balas.size(); i++) {
-                if (balas.get(i) == escolha) {
-                    balas.remove(i);
-                }
-                if (roleta == escolha) {
-                    System.out.println("Você morreu com um tiro!!");
-                    break;
-                } else if(balas.size() == 1){
-                    System.out.println("Só resta a bala de verdade agora!! Você venceu");
-                    break;
-                }
+            if (!sc.hasNextInt()) {
+                System.out.println("Digite apenas números!");
+                sc.next();
+                continue;
             }
 
+            escolha = sc.nextInt();
+
+            if (!balas.contains(escolha)) {
+                System.out.println("Essa bala já foi usada!");
+                continue;
+            }
+            if (escolha == roleta) {
+                System.out.println("Você morreu com um tiro!!");
+                break;
+            }
+
+            balas.remove(Integer.valueOf(escolha));
+            System.out.println("Sobreviveu...");
+        }
+
+        if (balas.size() == 1 && balas.get(0) == roleta) {
+            System.out.println("Só restou a bala mortal. Você venceu!");
         }
     }
 }
